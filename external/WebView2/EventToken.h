63f3622efdb6db8abadeca6c5290392c2e8602f0
GalaxyShard <dominic.adragna@byteroach.com>
2025-01-07 04:39:40 -0500

    add stub EventToken.h

---
+++ external/WebView2/EventToken.h
@@ -0,0 +1,6 @@
+// This file is a manually-written stub for EventToken.h
+
+// https://learn.microsoft.com/en-us/windows/win32/api/eventtoken/ns-eventtoken-eventregistrationtoken
+typedef struct EventRegistrationToken {
+  __int64 value;
+} EventRegistrationToken;
